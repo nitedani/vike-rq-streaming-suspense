a007bdd66a71e998e11c79bb58e69cd2e7336a5d
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-11-17 03:14:58 +0100

    update

--- src/pages/index/index.page.telefunc.ts
+++ src/pages/index/index.page.telefunc.ts
@@ -1,4 +1,6 @@
 export const onGetUsers = async () => {
+  console.log("onGetUsers");
+
   // simulate network delay
   await new Promise((r) => setTimeout(r, 500));
   return ["user1", "user2", "user3"];
